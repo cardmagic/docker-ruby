ff07ede29abc590675c27fb5e88230ace06000f4
Lucas Carlson <lucas@rufy.com>
2014-04-21 11:29:40 -0700

    Setup script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-git branch | cut -c3- | grep -v master | xargs git checkout | docker build -t cardmagic/ruby:`sed -n '/\* /s///p'` .
+git branch | cut -c3- | grep -v master | xargs -n 1 git checkout | docker build -t cardmagic/ruby:`sed -n '/\* /s///p'` .
